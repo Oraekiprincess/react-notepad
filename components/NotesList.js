351213003802143ef2533aba902c9829533b1beb
Oraekiprincess <114993346+Oraekiprincess@users.noreply.github.com>
2023-02-26 22:14:54 +0100

    Add files via upload

---
+++ components/NotesList.js
@@ -0,0 +1,13 @@
+import Note from './Note';
+
+const NotesList = ({ notes }) => {
+    return (
+        <div className='notes-list'>
+            {notes.map((note)=> (
+            <Note />
+            ))}
+        </div>
+    );
+};
+
+export default NotesList;
